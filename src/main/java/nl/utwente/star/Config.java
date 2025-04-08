c1ded7fadbeb8b262821de6f90252c7f618bb51a
Maarten <mjmarcusse@gmail.com>
2025-04-08 23:06:38 +0200

    make code a bit more clear

--- src/main/java/nl/utwente/star/Config.java
+++ src/main/java/nl/utwente/star/Config.java
@@ -6,24 +6,20 @@ import java.util.Properties;
 
 public class Config {
 
-    private static final Properties props = new Properties();
+    private static final Properties properties = new Properties();
 
     static {
         try (InputStream input = Config.class.getClassLoader().getResourceAsStream("config.properties")) {
             if (input == null) {
                 throw new RuntimeException("Unable to find config.properties");
             }
-            props.load(input);
-        } catch (IOException ex) {
-            throw new RuntimeException("Error reading config.properties", ex);
+            properties.load(input);
+        } catch (IOException exception) {
+            throw new RuntimeException("Error reading config.properties", exception);
         }
     }
 
     public static String get(String key) {
-        return props.getProperty(key);
-    }
-
-    public static String get(String key, String defaultValue) {
-        return props.getProperty(key, defaultValue);
+        return properties.getProperty(key);
     }
 }
